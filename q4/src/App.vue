63100972d46b68ccf4398a13ec352c66cee45fc5
Eldar <elderblr2@gmail.com>
2019-10-15 15:05:36 +0200

    Task 4: add axios, sass-loader

--- q4/src/App.vue
+++ q4/src/App.vue
@@ -17,4 +17,15 @@ export default {
 
 <style lang="scss">
   @import "sass/main";
+
+  body {
+    background-color: #fafaf3;
+    font-family: 'PT Sans', sans-serif;
+  }
+
+  #app {
+    box-shadow: 0px 2px 5px -2px rgba(0,0,0,0.75);
+    margin: 0 auto;
+    max-width: 1024px;
+  }
 </style>
