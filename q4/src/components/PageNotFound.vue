46d60a5b02c3de730d9d3a8db3c8abb1642143fc
Eldar <elderblr2@gmail.com>
2019-10-17 12:37:12 +0200

    Task 4: Add 404 page

---
+++ q4/src/components/PageNotFound.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <h1>Oops. Page not found. Come back to <router-link class="link" to="/">Home</router-link> page</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "PageNotFound"
+  }
+</script>
+
+<style scoped lang="scss">
+  .container {
+    display: flex;
+    min-height: 100vh;
+
+    h1 {
+      font-size: 3rem;
+      font-weight: bold;
+      margin: auto;
+    }
+
+    .link {
+      color: #00a2ff;
+      text-decoration: underline;
+    }
+  }
+</style>
--- q4/src/routes.js
+++ q4/src/routes.js
@@ -1,9 +1,11 @@
 import Listing from "./components/Listing";
 import Details from "./components/Details";
+import PageNotFound from "./components/PageNotFound";
 
 const routes = [
   { path: '/', component: Listing },
   { path: '/post/:slug', component: Details },
+  { path: "*", component: PageNotFound }
 ];
 
 export default routes;
