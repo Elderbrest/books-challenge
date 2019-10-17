13bec4958ba814bb6e7b0217da3c070fbec7938e
Eldar <elderblr2@gmail.com>
2019-10-17 17:01:07 +0200

    Task 4: delete unused default image, clean up

--- q4/src/api.js
+++ q4/src/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API_URL = 'http://localhost:3000'
+const API_URL = 'http://localhost:3000';
 
 const getBooks = () => axios.get(`${API_URL}/books`);
 
@@ -8,4 +8,4 @@ const getBookBySlug = slug => axios.get(`${API_URL}/books/${slug}`);
 export {
   getBooks,
   getBookBySlug
-}
+};
